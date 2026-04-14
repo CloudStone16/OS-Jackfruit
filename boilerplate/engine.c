8518e968aa2d11f5a1cdd73f3c3f888869ec9524
ArjunGK28 <arjungk28@gmail.com>
2026-04-14 20:02:35 +0530

    fix: implement bounded buffer push and pop functions with shutdown handling

--- boilerplate/engine.c
+++ boilerplate/engine.c
@@ -291,9 +291,23 @@ static void bounded_buffer_begin_shutdown(bounded_buffer_t *buffer)
  */
 int bounded_buffer_push(bounded_buffer_t *buffer, const log_item_t *item)
 {
-    (void)buffer;
-    (void)item;
-    return -1;
+     pthread_mutex_lock(&buffer->mutex);
+
+    while (!buffer->shutting_down && buffer->count == LOG_BUFFER_CAPACITY)
+        pthread_cond_wait(&buffer->not_full, &buffer->mutex);
+
+    if (buffer->shutting_down) {
+        pthread_mutex_unlock(&buffer->mutex);
+        return -1;
+    }
+
+    buffer->items[buffer->tail] = *item;
+    buffer->tail = (buffer->tail + 1) % LOG_BUFFER_CAPACITY;
+    buffer->count++;
+
+    pthread_cond_signal(&buffer->not_empty);
+    pthread_mutex_unlock(&buffer->mutex);
+    return 0;
 }
 
 /*
@@ -307,9 +321,23 @@ int bounded_buffer_push(bounded_buffer_t *buffer, const log_item_t *item)
  */
 int bounded_buffer_pop(bounded_buffer_t *buffer, log_item_t *item)
 {
-    (void)buffer;
-    (void)item;
-    return -1;
+    pthread_mutex_lock(&buffer->mutex);
+
+    while (buffer->count == 0 && !buffer->shutting_down)
+        pthread_cond_wait(&buffer->not_empty, &buffer->mutex);
+
+    if (buffer->count == 0 && buffer->shutting_down) {
+        pthread_mutex_unlock(&buffer->mutex);
+        return -1;
+    }
+
+    *item = buffer->items[buffer->head];
+    buffer->head = (buffer->head + 1) % LOG_BUFFER_CAPACITY;
+    buffer->count--;
+
+    pthread_cond_signal(&buffer->not_full);
+    pthread_mutex_unlock(&buffer->mutex);
+    return 0;
 }
 
 /*
@@ -587,11 +615,25 @@ int main(int argc, char *argv[])
     }
 
     if (strcmp(argv[1], "supervisor") == 0) {
-        if (argc < 3) {
-            fprintf(stderr, "Usage: %s supervisor <base-rootfs>\n", argv[0]);
-            return 1;
+         printf("[+] Supervisor started with rootfs: %s\n", argv[2]);
+
+        while (1) {
+            printf("engine> ");
+            fflush(stdout);
+
+            char input[256];
+
+            if (fgets(input, sizeof(input), stdin) == NULL) {
+                break;
+            }
+
+            // remove newline character
+            input[strcspn(input, "\n")] = '\0';
+
+            printf("[DEBUG] Command received: %s\n", input);
         }
-        return run_supervisor(argv[2]);
+
+        return 0;
     }
 
     if (strcmp(argv[1], "start") == 0)
